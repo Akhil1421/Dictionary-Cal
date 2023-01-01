840adf931eecae36f32bf531ed02622ad7f3ceba
Akhil Sharma <akhilsharma1421@gmail.com>
2023-01-01 18:38:16 +0530

    Corrections in - operator

--- src/Calculator.js
+++ src/Calculator.js
@@ -17,8 +17,14 @@ const Calculator = ()=>{
                              a[a.length-1] = a[a.length-1]+event.target.innerText;
                         }
                         else {
-                            if(a.length===1 && a[0]==="-"){
-                                a[0]+=event.target.innerText;
+                            if(a.length!==1 && a[a.length-1]==="-"){
+                                a[a.length-1]+=event.target.innerText;
+                                let c=a[a.length-1];
+                                a[a.length-1]="+"
+                                a.push(c)
+                            }
+                            else if(a.length===1 && a[0]==="-"){
+                                a[0]+=event.target.innerText
                             }
                             else a.push(event.target.innerText)
                         }
